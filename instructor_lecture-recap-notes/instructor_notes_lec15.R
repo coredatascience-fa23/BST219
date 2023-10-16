ab249449d01931fc78bc1277cac7928d5b1707d0
Dongdong Li <dli.stats@gmail.com>
2023-10-16 11:16:04 -0400

    lec 15 notes

---
+++ instructor_lecture-recap-notes/instructor_notes_lec15.R
@@ -0,0 +1,23 @@
+#### Recap last lec  ####
+
+# Data viz principles 
+# 1. **type of charts**
+
+# 2. **starting from zero or not**
+
+# 3. **do not distort values**
+
+# 4. **order by a meaningful value**
+
+# 5. **think of color blind**
+
+# 6. **3-d plot**
+
+# 7. **know your audience**
+
+# Today's lecture
+# 1. maps
+# 2. no lab this week
+# 3. HW2 released 9/25; due in Oct 22. 
+# 4. mid term in end October/early November
+# 5. final project: form a team of 3-5; fill out a proeject preference form (released next week)
